4b90e891f498ac47af01ab45060d0984197c9c88
timsch3 <timsch3@gmail.com>
2021-08-14 14:13:13 +0200

    fixed 25:01 showing after pomodoro

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,12 +31,12 @@ function stopTime() {
     let mins, secs
     mins = ~~(passedSecs % 3600 / 60)
     secs = ~~(passedSecs % 60)
-    if (mins < 25) {
+    if (mins < 25) { // mins < 25
         output.innerHTML = mins.toString().padStart(2, "0")+":"+secs.toString().padStart(2, "0")
     }
     else {
         output.style.color = "#999"
-        output.innerHTML = mins.toString().padStart(2, "0")+":"+secs.toString().padStart(2, "0")
+        output.innerHTML = "25:00"
         clearInterval(timeoutVar)
         timerStarted = false
         if (intervalCount == 1) {
